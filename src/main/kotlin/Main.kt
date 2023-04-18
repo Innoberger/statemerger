5ad879a200c349ee086a0ea4519af28d9d4b0f32
Innoberger <fynn.arnold@pm.me>
2023-04-18 15:58:46 +0200

    added states and capitals

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,12 +6,23 @@ fun main() {
 
     val states = States()
 
+    // first, we will create a state for each federal state of Germany only containing it's capital
     states.makeState("Stuttgart", "Baden-Württemberg")
     states.makeState("München", "Bayern")
     states.makeState("BerlinStadt", "BerlinLand")
-
-    states.union("Baden-Württemberg", "Bayern")
-    states.union("Bayern", "BerlinLand")
+    states.makeState("Potsdam", "Brandenburg")
+    states.makeState("BremenStadt", "BremenLand")
+    states.makeState("HamburgStadt", "HamburgLand")
+    states.makeState("Wiesbaden", "Hessen")
+    states.makeState("Schwerin", "Mecklenburg-Vorpommern")
+    states.makeState("Hannover", "Niedersachsen")
+    states.makeState("Düsseldorf", "Nordrhein-Westfalen")
+    states.makeState("Mainz", "Rheinland-Pfalz")
+    states.makeState("Saarbrücken", "Saarland")
+    states.makeState("Dresden", "Sachsen")
+    states.makeState("Magdeburg", "Sachsen-Anhalt")
+    states.makeState("Kiel", "Schleswig-Holstein")
+    states.makeState("Erfurt", "Thüringen")
 
     println(states.states)
 

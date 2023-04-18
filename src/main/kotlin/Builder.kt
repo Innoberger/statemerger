2f01a49b1eaea3592c9b2c31ade190ed43834939
Innoberger <fynn.arnold@pm.me>
2023-04-18 14:38:26 +0200

    added Berlin

--- src/main/kotlin/Builder.kt
+++ src/main/kotlin/Builder.kt
@@ -5,9 +5,10 @@ class Builder {
         fun buildGermany(): Node {
             val bw = StateWrapper.makeState("Stuttgart", "Baden-Württemberg")
             val by = StateWrapper.makeState("München", "Bayern")
+            val be = StateWrapper.makeState("Berlin", "Berlin")
             // to be continued (alphabetical order) ...
 
-            return StateWrapper.union(bw, by)
+            return StateWrapper.union(StateWrapper.union(bw, by), be)
         }
     }
 }

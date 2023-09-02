479266334be0781ca3467ae10c032a1cfb1ddf00
Innoberger <fynn.arnold@pm.me>
2023-09-02 14:54:53 +0200

    added navigation bar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,16 +1,67 @@
 <script>
+	// @ts-nocheck
 
+	import {
+		Styles,
+		Collapse,
+		Navbar,
+		NavbarToggler,
+		NavbarBrand,
+		Nav,
+		NavItem,
+		NavLink,
+		Icon
+	} from 'sveltestrap';
+
+	let isOpen = false;
+
+	function handleUpdate(event) {
+		isOpen = event.detail.isOpen;
+	}
+
+	const pages = [
+		{
+			"title": "Start",
+			"link": "",
+			"icon": "house-door-fill"
+		},
+		{
+			"title": "Karte",
+			"link": "map",
+			"icon": "map-fill"
+		},
+		{
+			"title": "Baum",
+			"link": "tree",
+			"icon": "tree-fill"
+		},
+		{
+			"title": "Hilfe",
+			"link": "help",
+			"icon": "question-lg"
+		}
+	]
 </script>
 
-<div>
-	<main>
-		<!-- Page content -->
-		<slot></slot>
-	</main>
+<Styles />
 
-	<ul>
-		<li>Bottom</li>
-		<li>Navigation</li>
-		<li>Bar</li>
-	</ul>
-</div>
+<Navbar color="dark" dark expand="md">
+	<NavbarBrand href="/">StateMerger</NavbarBrand>
+	<NavbarToggler on:click={() => (isOpen = !isOpen)} />
+	<Collapse {isOpen} navbar expand="md" on:update={handleUpdate}>
+		<Nav class="ms-auto" navbar>
+			{#each pages as page}
+			<NavItem>
+				<NavLink href="/{page.link}">
+					<Icon name={page.icon}/>
+					<span>{page.title}</span>
+				</NavLink>
+			</NavItem>
+			{/each}
+		</Nav>
+	</Collapse>
+</Navbar>
+
+<main>
+	<slot></slot>
+</main>

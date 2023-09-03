fa0ac2dfce123df15eed3beb7663665242e936b9
Innoberger <fynn.arnold@pm.me>
2023-09-03 21:11:38 +0200

    main page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -18,41 +18,48 @@
 	<meta name="description" content={currentPage.description} />
 </svelte:head>
 
-<nav class="navbar navbar-dark bg-dark sticky-top navbar-expand-lg">
-	<div class="container-md">
-		<strong><a class="navbar-brand" href="/">StateMerger</a></strong>
-		<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-			<span class="navbar-toggler-icon"></span>
-		</button>
-		<div class="collapse navbar-collapse" id="navbarNav">
-			<ul class="navbar-nav ms-auto">
-				{#each data.pages as page}
-					<li class="nav-item">
-						<a class="nav-link {currentPage.link === page.link ? 'active' : ''}" aria-current="page" href="/{page.link}">
-							<i class="bi-{page.icon}"></i>
-							<span>{page.title}</span>
-						</a>
-					</li>
-				{/each}
-			</ul>
-		</div>
-	</div>
-  </nav>
 
-<main class="py-4 mb-5">
-	<div class="container-md">
-		{#if currentPage.headline}
-			<h1>{currentPage.title}</h1>
-			<hr/>
-		{/if}
-		<div class="pt-1">
-			<slot></slot>
-		</div>
-    </div>
-</main>
+{#if currentPage.headline}
 
-<footer class="bg-dark text-secondary fixed-bottom py-3 mt-5">
-	<div class="container-md">
-		<strong>StateMerger:</strong> Optimierung der Bundesrepublik. Eine Projektarbeit an der <a class="link-secondary" href="https://www.h-ka.de/">Hochschule Karlsruhe</a>.
-	</div>
-</footer>
+	<nav class="navbar navbar-dark bg-dark sticky-top navbar-expand-lg">
+		<div class="container-md">
+			<strong><a class="navbar-brand" href="/">StateMerger</a></strong>
+			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarNav">
+				<ul class="navbar-nav ms-auto">
+					{#each data.pages as page}
+						<li class="nav-item">
+							<a class="nav-link {currentPage.link === page.link ? 'active' : ''}" aria-current="page" href="/{page.link}">
+								<i class="bi-{page.icon}"></i>
+								<span>{page.title}</span>
+							</a>
+						</li>
+					{/each}
+				</ul>
+			</div>
+		</div>
+	</nav>
+
+	<main class="py-4 mb-5">
+		<div class="container-md">
+				<h1>{currentPage.title}</h1>
+				<hr/>
+			<div class="pt-1">
+				<slot></slot>
+			</div>
+		</div>
+	</main>
+
+	<footer class="bg-dark text-secondary fixed-bottom py-3 mt-5">
+		<div class="container-md">
+			<strong>StateMerger:</strong> Optimierung der Bundesrepublik. Eine Projektarbeit an der <a class="link-secondary" href="https://www.h-ka.de/">Hochschule Karlsruhe</a>.
+		</div>
+	</footer>
+
+{:else }
+
+	<slot></slot>
+
+{/if}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,12 +2,12 @@
 
 </script>
 
-<header class="masthead text-center text-black">
+<header class="masthead text-center text-white">
 	<div class="masthead-content">
 		<div class="container px-5">
-			<h1 class="masthead-heading mb-0">State Merger</h1>
+			<h1 class="masthead-heading mb-0">StateMerger</h1>
 			<h2 class="masthead-subheading mb-0">Optimierung der Bundesrepublik</h2>
-			<a class="btn btn-primary btn-xl rounded-pill mt-5" href="#scroll">Jetzt starten</a>
+			<a class="btn btn-outline-light mt-5 px-5 py-3" href="/map">Jetzt starten</a>
 		</div>
 	</div>
 	<div class="bg-circle-1 bg-circle"></div>
@@ -15,3 +15,71 @@
 	<div class="bg-circle-3 bg-circle"></div>
 	<div class="bg-circle-4 bg-circle"></div>
 </header>
+
+<style>
+	header.masthead {
+		position: relative;
+		overflow: hidden;
+		padding-top: calc(10rem + 72px);
+		padding-bottom: 7rem;
+		height: 100vh;
+		background: linear-gradient(0deg, #ff6a00 0%, #ee0979 100%);
+		background-repeat: no-repeat;
+		background-position: center center;
+		background-attachment: scroll;
+		background-size: cover;
+	}
+	header.masthead .masthead-content {
+		z-index: 1;
+		position: relative;
+	}
+	header.masthead .masthead-content .masthead-heading {
+		font-size: 4rem;
+	}
+	header.masthead .masthead-content .masthead-subheading {
+		font-size: 2rem;
+	}
+	header.masthead .bg-circle {
+		z-index: 0;
+		position: absolute;
+		border-radius: 100%;
+		background: linear-gradient(0deg, #ee0979 0%, #ff6a00 100%);
+	}
+	header.masthead .bg-circle-1 {
+		height: 90rem;
+		width: 90rem;
+		bottom: -55rem;
+		left: -55rem;
+	}
+	header.masthead .bg-circle-2 {
+		height: 50rem;
+		width: 50rem;
+		top: -25rem;
+		right: -25rem;
+	}
+	header.masthead .bg-circle-3 {
+		height: 20rem;
+		width: 20rem;
+		bottom: -10rem;
+		right: 5%;
+	}
+	header.masthead .bg-circle-4 {
+		height: 30rem;
+		width: 30rem;
+		top: -5rem;
+		right: 35%;
+	}
+	
+	@media (min-width: 992px) {
+		header.masthead {
+			padding-top: calc(10rem + 55px);
+			padding-bottom: 10rem;
+		}
+		header.masthead .masthead-content .masthead-heading {
+			font-size: 6rem;
+		}
+		header.masthead .masthead-content .masthead-subheading {
+				font-size: 4rem;
+		}
+	}
+</style>

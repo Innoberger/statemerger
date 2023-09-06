797dc5a6226d9d1a7aae5d647931d5a15ba73427
Innoberger <fynn.arnold@pm.me>
2023-09-06 21:22:09 +0200

    can now show a graph for a specific single country

--- src/lib/components/Tree.svelte
+++ src/lib/components/Tree.svelte
@@ -1,7 +1,15 @@
 <script lang="ts">
-	// @ts-nocheck
-
+	// @ts-ignore
+	import { selectedCountryStatesForest } from '$lib/stores/selected-country';
 	import * as d3 from 'd3';
+
+	interface TreeNode {
+		name: string;
+		value: number;
+		type: string;
+		level: string;
+		children?: TreeNode[]; // Optional for children
+	}
 
 	/**
 	 * ---
@@ -13,132 +21,125 @@
 	 * ---
 	 */
 
-	export let treeData = {
-		"name": "Eve",
-		"value": 15,
-		"type": "black",
-		"level": "yellow",
-		"children": [
-			{
-				"name": "Cain",
-				"value": 10,
-				"type": "grey",
-				"level": "red"
-			},
-			{
-				"name": "Seth",
-				"value": 10,
-				"type": "grey",
-				"level": "red",
-				"children": [
-					{
-						"name": "Enos",
-						"value": 7.5,
-						"type": "grey",
-						"level": "purple"
-					},
-					{
-						"name": "Noam",
-						"value": 7.5,
-						"type": "grey",
-						"level": "purple"
-					}
-				]
-			},
-			{
-				"name": "Abel",
-				"value": 10,
-				"type": "grey",
-				"level": "blue"
-			},
-			{
-				"name": "Awan",
-				"value": 10,
-				"type": "grey",
-				"level": "green",
-				"children": [
-					{
-						"name": "Enoch",
-						"value": 7.5,
-						"type": "grey",
-						"level": "orange"
-					}
-				]
-			},
-			{
-				"name": "Azura",
-				"value": 10,
-				"type": "grey",
-				"level": "green"
-			}
-		]
+	let treeData;
+	let nodes = undefined;
+
+	function transformPredecessorMap(predecessorMap: { [key: string]: string }): TreeNode {
+		function getChildNodes(nodeName: string): TreeNode[] | undefined {
+			return Object.entries(predecessorMap).filter(([node, parent]) => {
+				return node !== parent && parent === nodeName
+			}).map(([node, _]) => {
+				return {
+					name: node,
+					value: 15, // You can set default values here
+					type: "black", // You can set default types here
+					level: "yellow" // You can set default levels here
+				} as TreeNode
+			})
+		}
+
+		function getRootNode(): TreeNode {
+			let name = Object.entries(predecessorMap).find(([node, parent]) => {
+				return	parent === node
+			})?.[0]
+
+			return {
+				name: name,
+				value: 15, // You can set default values here
+				type: "black", // You can set default types here
+				level: "yellow" // You can set default levels here
+			} as TreeNode
+		}
+
+		function setChildNodes(node: TreeNode): TreeNode {
+			let descendants = getChildNodes(node.name)?.map((childNode) => setChildNodes(childNode))
+
+			if (descendants && descendants.length) node.children = descendants
+
+			return node
+		}
+
+		let tree: TreeNode = getRootNode()
+
+		tree = setChildNodes(tree)
+
+		return tree
+	}
+
+	const predecessorMap = {
+		"Stuttgart": "Baden-Württemberg",
+		"Baden-Württemberg": "Baden-Württemberg",
+		"Mannheim": "7e86eb1f-b49f-494d-a7db-a51cf1bddce4",
+		"7e86eb1f-b49f-494d-a7db-a51cf1bddce4": "Baden-Württemberg",
+		"Karlsruhe": "a47871fd-485a-45c0-8c3a-300f83eade7b",
+		"a47871fd-485a-45c0-8c3a-300f83eade7b": "Baden-Württemberg"
 	};
-
-	/*
-	count.subscribe((value) => {
-		console.log(value);
-	});*/
-
-	/**
-	 * TODOs:
-	 * 	1. create the svg within the content, not the script tag; that's why we have svelte!
-	 * 	2. get rid of the typescript errors (might be solved in task 1)
-	 *  3. load our disjoint-set forest model with the config parser instead of this dummy data
-	 */
 
 	// set the dimensions and margins of the diagram
 	const	margin = {top: 20, right: 90, bottom: 30, left: 90},
-			width  = 660 - margin.left - margin.right,
-			height = 500 - margin.top - margin.bottom;
+			width  = 1000 - margin.left - margin.right,
+			height = 600 - margin.top - margin.bottom;
 
 	// declares a tree layout and assigns the size
 	const treemap = d3.tree().size([height, width]);
+	
+	selectedCountryStatesForest.subscribe((value) => {
+		if (!(value && value.states && value.states.predecessor)) return;
+		refreshTree()
+	});
+	
+	function refreshTree() {
+		treeData = transformPredecessorMap(predecessorMap)
+		//  assigns the data to a hierarchy using parent-child relationships
+		nodes = d3.hierarchy(treeData);
 
-	//  assigns the data to a hierarchy using parent-child relationships
-	let nodes = d3.hierarchy(treeData);
-	nodes = treemap(nodes);
+		//
+		nodes = treemap(nodes)
+	}
 </script>
 
-<div id="tree-container">
-	<svg
-		id="svg-container"
-		width={width + margin.left + margin.right}
-		height={height + margin.top + margin.bottom}
-	>
-		<g id="svg-group" transform="translate({margin.left},{margin.top})">
-			{#each nodes.descendants().slice(1) as edge}
-				<path
-					class="link"
-					style="stroke: {edge.data.level}"
-					d="M{edge.y},{edge.x}C{(edge.y + edge.parent.y) / 2},{edge.x} {(edge.y + edge.parent.y) / 2}, {edge.parent.x} {edge.parent.y},{edge.parent.x}"
-				/>
-			{/each}
-
-			{#each nodes.descendants() as node}
-				<!-- svelte-ignore a11y-click-events-have-key-events -->
-				<!-- svelte-ignore a11y-no-static-element-interactions -->
-				<g
-					class="node {node.children ? " node--internal" : " node--leaf"}"
-					transform="translate({node.y},{node.x})"
-					on:click={() => alert(node.data.name)}
-				>
-					<circle
-						r={node.data.value}
-						style="fill: {node.data.level}"
+{#if nodes}
+	<div id="tree-container">
+		<svg
+			id="svg-container"
+			width={width + margin.left + margin.right}
+			height={height + margin.top + margin.bottom}
+		>
+			<g id="svg-group" transform="translate({margin.left},{margin.top})">
+				{#each nodes.descendants().slice(1) as edge}
+					<path
+						class="link"
+						style="stroke: {edge.data.level}"
+						d="M{edge.y},{edge.x}C{(edge.y + edge.parent.y) / 2},{edge.x} {(edge.y + edge.parent.y) / 2}, {edge.parent.x} {edge.parent.y},{edge.parent.x}"
 					/>
-					<text
-						dy=".35em"
-						x={node.children ? (node.data.value + 5) * -1 : node.data.value + 5}
-						y={node.children && node.depth !== 0 ? -(node.data.value + 5) : node}
-						text-anchor={node.children ? "end" : "start"}
+				{/each}
+
+				{#each nodes.descendants() as node}
+					<!-- svelte-ignore a11y-click-events-have-key-events -->
+					<!-- svelte-ignore a11y-no-static-element-interactions -->
+					<g
+						class="node {node.children ? " node--internal" : " node--leaf"}"
+						transform="translate({node.y},{node.x})"
+						on:click={() => alert(node.data.name)}
 					>
-						{node.data.name}
-					</text>
-				</g>
-			{/each}
-		</g>
-	</svg>
-</div>
+						<circle
+							r={node.data.value}
+							style="fill: {node.data.level}"
+						/>
+						<text
+							dy=".35em"
+							x={node.children ? (node.data.value + 5) * -1 : node.data.value + 5}
+							y={node.children && node.depth !== 0 ? -(node.data.value + 5) : node}
+							text-anchor={node.children ? "end" : "start"}
+						>
+							{node.data.name}
+						</text>
+					</g>
+				{/each}
+			</g>
+		</svg>
+	</div>
+{/if}
 
 <style>
 	g {
--- src/routes/tree/+page.svelte
+++ src/routes/tree/+page.svelte
@@ -1,11 +1,8 @@
 <script lang="ts">
-	import { selectedCountryStatesForest } from '$lib/stores/selected-country';
 	import CountrySelectorButton from '$lib/components/CountrySelectorButton.svelte';
 	import Tree from '$lib/components/Tree.svelte';
 </script>
 
 <CountrySelectorButton />
-
-{JSON.stringify($selectedCountryStatesForest)}
 
 <Tree />

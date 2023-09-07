2e18cd9cb7c8a52e20e0de01ccd6270419c9658b
Innoberger <fynn.arnold@pm.me>
2023-09-07 19:14:23 +0200

    added function to retrieve predecessormap

--- src/lib/components/graph/CountryForest.svelte
+++ src/lib/components/graph/CountryForest.svelte
@@ -2,6 +2,7 @@
     import StateTree from './StateTree.svelte';
 	import { selectedCountryConfigJson, selectedCountryStatesForest } from '$lib/stores/selected-country';
 	import type { State } from '$lib/parser/state';
+	import type { States } from '$lib/model/states';
 
     const predecessorMap = {
 		"Stuttgart": "Baden-Württemberg",
@@ -12,13 +13,29 @@
 		"a47871fd-485a-45c0-8c3a-300f83eade7b": "Baden-Württemberg"
 	};
 
-	function getIsolatedPredecessorMap(state: State): { [key: string]: string } {
-		return predecessorMap
+    function findRootNoPathCompression(forest: States, node: string): string | undefined {
+        /*
+		 * Recursively walk up the tree,
+		 * but do not apply path compression.
+		 * This is for visualization purposes only
+		 */
+        if (node !== forest.states.predecessor[node]) {
+            return findRootNoPathCompression(forest, forest.states.predecessor[node]!)!;
+        }
+
+        return forest.states.predecessor[node];
+    }
+
+	function getFilteredPredecessorMap(state: State): { [key: string]: string } {
+		const filteredPredecessors = Object.entries($selectedCountryStatesForest.states.predecessor)
+			.filter(([_name, _]) => findRootNoPathCompression($selectedCountryStatesForest, _name) === state.name)
+
+		return Object.fromEntries(filteredPredecessors)
 	}
 </script>
 
 {#if $selectedCountryConfigJson?.states}
     {#each $selectedCountryConfigJson.states as state}
-        <StateTree predecessorMap={getIsolatedPredecessorMap(state)} />
+        <StateTree predecessorMap={getFilteredPredecessorMap(state)} />
     {/each}
 {/if}

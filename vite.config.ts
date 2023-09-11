579cbfd9803dddf9291e5df93f9cf87d8a468c3a
Innoberger <fynn.arnold@pm.me>
2023-09-11 19:26:11 +0200

    made ready for production

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,30 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
+import copy from 'rollup-plugin-copy'
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [
+		sveltekit(),
+		copy({
+			targets: [
+				{
+					src: 'node_modules/bootstrap/dist/**/*',
+					dest: 'static/vendor/bootstrap'
+				},
+				{
+					src: 'node_modules/bootstrap-icons/**/*',
+					dest: 'static/vendor/bootstrap-icons'
+				},
+			]
+		})
+	],
 
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	},
 
 	css: {
+		devSourcemap: true,
 		preprocessorOptions: {
 			scss: {
 				additionalData: '@use "src/variables.scss" as *;'
